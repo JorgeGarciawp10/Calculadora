b5a9da7bd7ce5794d96cc72304bfc28d76aa3d29
AdrianSolerRodriguez <134058061+AdrianSolerRodriguez@users.noreply.github.com>
2023-05-26 10:04:18 +0200

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -11,15 +11,7 @@ public class Calculadora {
 
 		int opcion = 0;
 
-		System.out.println("1. Sumar ");
-		System.out.println("2. Restar 2");
-		System.out.println("3. Multiplicar 3");
-		System.out.println("4. Dividir");
-		System.out.println("5 Potencia");
-		System.out.println("6 Salir");
-
-		System.out.println("Escribe una de las opciones");
-		opcion = sc.nextInt();
+		
 		double numero = 0.0;
 		double numero1 = 0.0;
 		double numero2 = 0.0;
@@ -28,53 +20,60 @@ public class Calculadora {
 		boolean menu = false;
 
 		while (menu == false) {
+			System.out.println("1. Sumar ");
+			System.out.println("2. Restar 2");
+			System.out.println("3. Multiplicar 3");
+			System.out.println("4. Dividir");
+			System.out.println("5 Potencia");
+			System.out.println("6 Salir");
+
+			System.out.println("Escribe una de las opciones");
+			opcion = sc.nextInt();
 			switch (opcion) {
-			case 1: {
-				System.out.println("Dime el numero:");
-				numero1 = sc.nextDouble();
-				System.out.println("Dime otro numero para sumar:");
-				numero2 = sc.nextDouble();
-				suma = suma(numero1, numero2);
-				System.out.println("El resultado es: " + suma);
-				break;
-			}
-			case 2: {
-				double resta=0;
-		                double numero1=0;
-				double numero2=0;
-				System.out.println("Dime los númeroslos cuáles quieras restar:");
-				numero1= sc.nextDouble() ;
-				numero2=sc.nextDouble();
-				resta=(numero1 - numero2);
-	   			System.out.println(resta);
-				break;
-			}
-			case 3: {
-				break;
-			}
-			case 4: {
-				break;
-			}
-			case 5: {
-				double resulPotencia = 0;
+				case 1: {
+					System.out.println("Dime el numero:");
+					numero1 = sc.nextDouble();
+					System.out.println("Dime otro numero para sumar:");
+					numero2 = sc.nextDouble();
+					suma = suma(numero1, numero2);
+					System.out.println("El resultado es: " + suma);
+					break;
+				}
+				case 2: {
+					System.out.println("Dime el numero:");
+					numero1 = sc.nextDouble();
+					System.out.println("Dime otro numero para restar:");
+					numero2 = sc.nextDouble();
+					resta = resta(numero1, numero2);
+					System.out.println("El resultado es: " + resta);
+					break;
+				}
+				case 3: {
+					break;
+				}
+				case 4: {
+					break;
+				}
+				case 5: {
+					double resulPotencia = 0;
 
-				System.out.println("Dime el numero:");
-				numero = sc.nextDouble();
-				System.out.println("Dime a que lo quieres elevar:");
-				potencia = sc.nextDouble();
-				resulPotencia = potencia(numero, potencia);
+					System.out.println("Dime el numero:");
+					numero = sc.nextDouble();
+					System.out.println("Dime a que lo quieres elevar:");
+					potencia = sc.nextDouble();
+					resulPotencia = potencia(numero, potencia);
 
-				System.out.println("El resultado es " + resulPotencia);
+					System.out.println("El resultado es " + resulPotencia);
 
-				break;
-			}
-			case 6: {
-				menu = true;
-				break;
-			}
-			default:
-				throw new IllegalArgumentException("Valor incorrecto: " + opcion);
-			}
+					break;
+				}
+				case 6: {
+					menu = true;
+					break;
+				}
+				default:
+					throw new IllegalArgumentException("Valor incorrecto: " + opcion);
+				}
 
 		}
 	}
@@ -90,6 +89,22 @@ public class Calculadora {
 
 		}
 		return resultado;
+
+	}
+	
+	
+	public static double resta(double numero1, double numero2) {
+		double resultado = 0;
+		try {
+
+			resultado = numero1 - numero2;
+
+		} catch (Exception e) {
+			System.out.println("Error");
+
+		} finally {
+			return resultado;
+		}
 
 	}
 
